0f908e24d2a604e5b09beef21fa2d857fb21f37e
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-02-21 09:24:48 -0800

    added Scripts and prefabs to MAHHH folder

---
+++ Assets/Henry/Script/SelectionStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionStatus : MonoBehaviour
+{
+    public bool iSeeMouse;
+    public bool iAmSelected;
+    // Use this for initialization
+    void Start()
+    {
+        iSeeMouse = false;
+        iAmSelected = false;
+    }
+
+    private void OnMouseEnter()
+    {
+        iSeeMouse = true;
+    }
+    private void OnMouseExit()
+    {
+        iSeeMouse = false;
+    }
+    private void OnMouseDown()
+    {
+        if (iAmSelected == false)
+        {
+            Debug.Log("I been Click");
+            iAmSelected = true;
+            GetComponentInChildren<GunScript>().readyToMove();
+        }
+        else
+        {
+            iAmSelected = false;
+            GetComponentInChildren<GunScript>().readyToMove();
+        }
+    }
+    public void ClearSelection()
+    {
+        iAmSelected = false;
+    }
+}
