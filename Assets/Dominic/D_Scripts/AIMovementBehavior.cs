47a012e5a2329fe544220e2b7114ee5f11ea67b7
Dominic Lagle <dominic.lagle444@gmail.com>
2018-03-06 12:43:15 -0800

    Basically changed nothin

--- Assets/Dominic/D_Scripts/AIMovementBehavior.cs
+++ Assets/Dominic/D_Scripts/AIMovementBehavior.cs
@@ -2,48 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIMovementBehavior : MonoBehaviour {
+public class AIMovementBehavior : MonoBehaviour
+{
 
-	public Transform targetMoveToObject;
-	public float maxMovementSpeed = 7.5f;
-	public float minMovementSpeed = 3.5f;
+    public Transform targetMoveToObject;
+    public float maxMovementSpeed = 7.5f;
+    public float minMovementSpeed = 3.5f;
 
 
-	private int health;
-	private int maxHealth = 100;
-	private int minHealth = 0;
-	private float step;
+    private int health;
+    private int maxHealth = 100;
+    private int minHealth = 0;
+    private float step;
 
-	void Start () {	
-		step = Random.Range(minMovementSpeed, maxMovementSpeed);
-		health = maxHealth;
-		StartCoroutine(MoveEnemy());
+    void Start()
+    {
+        step = Random.Range(minMovementSpeed, maxMovementSpeed);
+        health = maxHealth;
+        //StartCoroutine(MoveEnemy());
+    }
 
-	}
-	IEnumerator MoveEnemy(){
-		while(transform.position != targetMoveToObject.position){
-			transform.position = Vector2.MoveTowards(transform.position, targetMoveToObject.position, step);
+    /*IEnumerator MoveEnemy(){
+		while(transform.position != targetMoveToObject.transform.position){
+			transform.position = Vector3.MoveTowards(transform.position, targetMoveToObject.transform.position, step * Time.deltaTime);
+			Debug.Log(transform.position);
 			yield return null;
 		}
-	}
-	public void OnCollisionEnter2D(Collision2D otherCollider){
-		if(otherCollider.gameObject.tag == "HomeBase"){
-			Die();
-		}
-		else if ( otherCollider.gameObject.tag == "regBullet") {
-			TakeDamage(50);
-			Debug.Log(health);
-		}
-	}
-	public void TakeDamage(int damage){
-		health -= damage;
-		if(health <= minHealth){
-			Die();
-		}
+	}*/
 
-	}
-	private void Die(){
-		DestroyObject(gameObject);
-	}
+    void Update()
+    {
+      step = step * Time.deltaTime;
+	  transform.position = Vector3.MoveTowards(transform.position, targetMoveToObject.position, step);  
+    }
+
+    public void OnCollisionEnter2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.tag == "HomeBase")
+        {
+            Die();
+        }
+        else if (otherCollider.gameObject.tag == "regBullet")
+        {
+            TakeDamage(50);
+            Debug.Log(health);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= minHealth)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        DestroyObject(gameObject);
+    }
 }
- 
--- Assets/Dominic/D_Scripts/SpawnEnemyBehaviour.cs
+++ Assets/Dominic/D_Scripts/SpawnEnemyBehaviour.cs
@@ -10,14 +10,16 @@ public class SpawnEnemyBehaviour : MonoBehaviour {
 	public float startWait;
 	public float spawnWait;
 	public float waveWait;
+	public int minEnemyCount = 10;
+	public int maxEnemyCount = 20;
 
 	private int index;
 	private int enemyCount;
+	private int waveCount;
 
 
 	IEnumerator SpawnEnemyCoroutine(){	
-		int minEnemyCount = 10;
-		int maxEnemyCount = 50;
+		
 		enemyCount = Random.Range(minEnemyCount, maxEnemyCount);
 		yield return new WaitForSeconds(startWait);
 		while (true){
@@ -36,28 +38,27 @@ public class SpawnEnemyBehaviour : MonoBehaviour {
 	void Update()
 	{
 		index = Random.Range(0, enemySpawnLocations.Count);
-		Debug.Log(index);	
 	}
 	void SpawnEnemy(){
 		if (index == 0){
 			float minX = -10.0f;
 			float maxX = 10.0f;
 			float randomX = Random.Range(minX, maxX);
-			Vector2 spawnPoint0 = new Vector2(randomX, enemySpawnLocations[0].transform.position.y);
+			Vector2 spawnPoint0 = new Vector3(randomX, enemySpawnLocations[0].transform.position.y, enemySpawnLocations[0].transform.position.z);
 			Instantiate(enemyToSpawn, spawnPoint0, Quaternion.identity);
 		}
 		else if (index == 1){
 			float minY = 0.0f;
 			float maxY = 10.0f;
 			float randomY = Random.Range(minY, maxY);
-			Vector2 spawnPoint1 = new Vector2(enemySpawnLocations[1].transform.position.x, randomY);
+			Vector2 spawnPoint1 = new Vector3(enemySpawnLocations[1].transform.position.x, randomY, enemySpawnLocations[1].transform.position.z);
 			Instantiate(enemyToSpawn, spawnPoint1, Quaternion.identity);				
 		}
 		else if (index == 2){
 			float minY = 0.0f;
 			float maxY = 10.0f;
 			float randomY = Random.Range(minY, maxY);
-			Vector2 spawnPoint2 = new Vector2(enemySpawnLocations[2].transform.position.x, randomY);
+			Vector2 spawnPoint2 = new Vector3(enemySpawnLocations[2].transform.position.x, randomY, enemySpawnLocations[2].transform.position.z);
 			Instantiate(enemyToSpawn, spawnPoint2, Quaternion.identity);
 		}
 	}		
