551e02a62875321df84b318502fff2613a4fda26
GALAXYCANNON\Matthew Casillano <MatthewAC@outlook.com>
2018-03-15 14:18:10 -0700

    Rotating Sky

---
+++ Assets/Matthew/BackGrounds/SkyRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyRotation : MonoBehaviour {
+
+    public float cur_Deg = 0f;
+    public float max_Deg = 360f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        cur_Deg += 1f;
+        transform.Rotate(eulerAngles: Vector3.forward * Time.deltaTime);
+       // if (Input.GetKeyDown("space"))
+       
+
+    }
+
+    void Degreset(float degrees)
+    {
+       
+    }
+}
