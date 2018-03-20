04d898457c2884905546f981b631321a8dd5e301
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-03-20 10:26:24 -0700

    Fixing again

---
+++ Assets/Henry's Package 1.0/TileMap/Scripts/GridSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSetup : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public int tileMapWidth;
+    public int tileMapHeight;
+    public Transform[,] tileMap;
+    void Start()
+    {
+        tileMap = new Transform[tileMapWidth, tileMapHeight];
+        for (int y = 0; y < tileMapHeight; y++)
+        {
+            for (int x = 0; x < tileMapWidth; x++)
+            {
+                Transform tile = Instantiate(tilePrefab);
+                tile.transform.parent = transform;
+                tile.transform.localPosition = new Vector3(x,y,0);
+                tile.transform.rotation = Quaternion.identity;
+                tileMap[x, y] = tile;
+            }
+        }
+    }
+}
