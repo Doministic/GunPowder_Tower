5b9b32ed4b216dc335e2f45bcadb7147ac43867c
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-03-20 10:59:08 -0700

    commit issue for game manager (Timer) Removed plugin

--- Assets/Dominic/D_Scripts/UpdateUIBehaviour.cs
+++ Assets/Dominic/D_Scripts/UpdateUIBehaviour.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UpdateUIBehaviour : MonoBehaviour {
+public class UpdateUIBehaviour : MonoBehaviour
+{
 
-	[SerializeField]
-	private Text timerLabel;
+    [SerializeField]
+    private Text timerLabel;
 
-	void Update () {
-		timerLabel.text = ForamtTime (GameManager.Instance.TimeRemaining);
-	}
+    void Update()
+    {
+        // timerLabel.text = ForamtTime(GameManager.Instance.TimeRemaining);
+    }
 
-	private string ForamtTime(float timeInSeconds){
-		return string.Format("{0}:{1:00}", Mathf.FloorToInt(timeInSeconds/60), Mathf.FloorToInt(timeInSeconds % 60));
-	}
+    private string ForamtTime(float timeInSeconds)
+    {
+        return string.Format("{0}:{1:00}", Mathf.FloorToInt(timeInSeconds / 60), Mathf.FloorToInt(timeInSeconds % 60));
+    }
 }
